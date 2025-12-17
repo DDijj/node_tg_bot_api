e294ade982b3ed67246d73def2fc2380f7645621
DDIJJ <sky088950403@gmail.com>
2025-12-17 23:43:55 +0800

    feat: add /start and /help commands

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import TelegramBot from 'node-telegram-bot-api';
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    'Bot is running.\nUse /help to see available commands.'
+  );
+});
+
+bot.onText(/\/help/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    '/start - check bot status\n/help - show commands'
+  );
+});
+
 // bot.on('message', (msg) => {
 //   bot.sendMessage(msg.chat.id, 'Bot is alive.');
 // });

0aebb82ab43bbb172747c2d79afa08e9042bf193
DDIJJ <sky088950403@gmail.com>
2025-12-17 23:28:09 +0800

    feat: handle text and sticker messages separately

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import 'dotenv/config';
 import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Bot is alive.');
+// bot.on('message', (msg) => {
+//   bot.sendMessage(msg.chat.id, 'Bot is alive.');
+// });
+bot.on('text', (msg) => {
+  bot.sendMessage(msg.chat.id, `You said: ${msg.text}`);
+});
+
+bot.on('sticker', (msg) => {
+  bot.sendMessage(msg.chat.id, 'I see a sticker 👀');
 });

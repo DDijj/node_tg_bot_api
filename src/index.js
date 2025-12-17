624c2e66adbf082e9a135080234bd8f1eaec1927
DDIJJ <sky088950403@gmail.com>
2025-12-17 22:41:58 +0800

    wip: current working state before api fix

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, 'Bot is alive.');
+});

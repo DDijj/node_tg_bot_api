e81e87f7c9469cb51a1324f71ec21a3b9494082b
DDIJJ <sky088950403@gmail.com>
2025-12-18 00:19:31 +0800

    feat: persist user state in json file

--- src/index.js
+++ src/index.js
@@ -1,49 +1,20 @@
-import 'dotenv/config';
-import axios from 'axios';
-import fs from 'fs/promises';
 import TelegramBot from 'node-telegram-bot-api';
+import 'dotenv/config';
+import fs from 'fs/promises';
+import startCmd from './commands/start.js';
+import helpCmd from './commands/help.js';
 
-const token = process.env.BOT_TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(
-    msg.chat.id,
-    'Bot is running.\nUse /help to see available commands.'
-  );
-});
 
-bot.onText(/\/help/, (msg) => {
-  bot.sendMessage(
-    msg.chat.id,
-    '/start - check bot status\n/help - show commands'
-  );
-});
+const STATE_FILE = 'data/state.json';
 
-bot.onText(/\/quote/, async (msg) => {
-  const res = await axios.get('https://api.quotable.io/random');
-  bot.sendMessage(msg.chat.id, res.data.content);
-});
+async function loadState() {
+  return JSON.parse(await fs.readFile(STATE_FILE, 'utf8'));
+}
 
-bot.on('sticker', async (msg) => {
-  const log = {
-    from: msg.from.username,
-    file_id: msg.sticker.file_id,
-    date: new Date().toISOString()
-  };
-
-  await fs.appendFile(
-    'sticker.log',
-    JSON.stringify(log) + '\n'
-  );
-
-  bot.sendMessage(msg.chat.id, 'Sticker logged.');
-});
-
-// bot.on('text', (msg) => {
-//   bot.sendMessage(msg.chat.id, `You said: ${msg.text}`);
-// });
-// 
-// bot.on('sticker', (msg) => {
-//   bot.sendMessage(msg.chat.id, 'I see a sticker 👀');
-// });
+async function saveState(state) {
+  await fs.writeFile(STATE_FILE, JSON.stringify(state, null, 2));
+}
+startCmd(bot);
+helpCmd(bot);

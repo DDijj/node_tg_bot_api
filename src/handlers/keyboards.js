91e997092d0a99ecb924746ec7173c2dd46e1681
DDIJJ <sky088950403@gmail.com>
2025-12-20 21:58:33 +0800

    add basic reply and prepare command

--- src/handlers/keyboards.js
+++ src/handlers/keyboards.js
@@ -18,18 +18,6 @@ export function registerKeyboards(bot) {
         });
     });
 }
-bot.sendMessage(chatId, '開啟遊戲', {
-    reply_markup: {
-        inline_keyboard: [[
-            {
-                text: '🎮 開啟 Mini App',
-                web_app: {
-                    url: 'https://sariah-superintolerable-bart.ngrok-free.dev/mini-app'
-                }
-            }
-        ]]
-    }
-});
 
 export function registerBuiltinKeyboards(bot) {
     bot.on('callback_query', (q) => {
